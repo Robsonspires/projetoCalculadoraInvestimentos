44de0cd0bb64e1869009e0df81a0428e5c3fadb0
Robsonspires <robsonspires@live.com>
2025-05-24 10:23:09 -0300

    Inserido as alternativas para o submit e para a propriedade name dos campos dentro de um formulário

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ import { generateReturnsArray } from "./src/investmentGoals.js";
 
 // const calculateButton = document.getElementById("calculate-results"); trocado do botão para o formulário.
 const form = document.getElementById("investment-form");
+const calculateButton = document.getElementById("calculate-results");
 
 function renderProgression(evt) {
   evt.preventDefault(); // Previne o comportamento padrão do formulário de recarregar a página.
-  const starttingAmount = Number(
-    document.getElementById("starting-amount").value
-  );
+  const starttingAmount = Number(form.elements["starting-amount"].value);
+  // const starttingAmount = Number(
+  //   document.getElementById("starting-amount").value
+  // );
   const additinalContribution = Number(
-    document.getElementById("additional-contribution").value
+    form.elements["additional-contribution"].value
   );
+  // const additinalContribution = Number(
+  //   document.getElementById("additional-contribution").value
+  // );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
   const returnRate = Number(document.getElementById("return-rate").value);
@@ -31,4 +36,5 @@ function renderProgression(evt) {
 }
 
 // calculateButton.addEventListener("click", renderProgression); // trocado do botão para o formulário.
-form.addEventListener("submit", renderProgression);
+// form.addEventListener("submit", renderProgression);
+calculateButton.addEventListener("click", renderProgression);
